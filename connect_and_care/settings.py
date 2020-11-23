440766916314c6dc0da37fe37961c7ee6134051d
Ode <odedahay@yahoo.com>
2020-11-24 00:41:50 +0800

    updated settings

--- connect_and_care/settings.py
+++ connect_and_care/settings.py
@@ -173,11 +173,11 @@ MEDIA_URL = '/media/'
 # SENDGRID_SANDBOX_MODE_IN_DEBUG=True
 
 # MailGun
-EMAIL_HOST = 'smtp.mailgun.org'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.mailgun.org'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
 
 # GMAIL
 # EMAIL_HOST = 'smtp.gmail.com'
